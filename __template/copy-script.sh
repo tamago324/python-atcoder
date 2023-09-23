7b14864f2ed4257d4a5dff4008b1a1a319eed8d7
tamago324 <tamago_pad@yahoo.co.jp>
2023-09-23 01:07:49 +0000

    カレントディレクトリをスクリプトファイルのディレクトリにする処理を入れた

--- __template/copy-script.sh
+++ __template/copy-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd `dirname $0`
 mkdir -p $(acc config-dir)/python && \
 cp ./template.json $(acc config-dir)/python/template.json && \
 cp ./main.py $(acc config-dir)/python/main.py && \
